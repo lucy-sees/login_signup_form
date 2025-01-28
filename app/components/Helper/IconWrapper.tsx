36df9069fe22b3511786d26ee3425f9b265e2b97
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2025-01-28 12:56:35 +0300

    Create IconWrapper.tsx

---
+++ app/components/Helper/IconWrapper.tsx
@@ -0,0 +1,11 @@
+const IconWrapper = ({ children }) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    width="32" 
+    height="32" 
+    fill="currentColor" 
+    viewBox="0 0 256 256"
+  >
+    {children}
+  </svg>
+);
